ef14c89a5e508fe507214dcef578583caa79c6de
Patrycja Galant <patrycja.ciruk@gmail.com>
2022-07-20 14:48:19 +0200

    Initial commit

---
+++ src/main/java/com/epam/admissionscommittee/AdmissionsCommitteeApplication.java
@@ -0,0 +1,13 @@
+package com.epam.admissionscommittee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmissionsCommitteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmissionsCommitteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/admissionscommittee/AdmissionsCommitteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.admissionscommittee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsCommitteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
